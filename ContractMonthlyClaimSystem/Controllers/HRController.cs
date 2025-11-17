d5afba9fa4b6ec1bd8384d947d44598084e7b947
Cindy GY <st10313401@imconnect.edu.za>
2025-11-17 14:32:17 +0200

    Create HRController
    Add a HRController

---
+++ ContractMonthlyClaimSystem/Controllers/HRController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[Authorize]
+public class HRController : Controller
+{
+    public IActionResult Index()
+    {
+        // 🎯 CHECK IF USER IS HR
+        var userRole = HttpContext.Session.GetUserRole();
+        if (!userRole.Equals("HR", StringComparison.OrdinalIgnoreCase))
+        {
+            return RedirectToAction("AccessDenied", "Account");
+        }
+
+        return View();
+    }
+
+    // Other HR actions...
+}
