970b3e4059f08ce7889aecdc893bb60e46521608
Cindy GY <st10313401@imconnect.edu.za>
2025-11-17 14:35:42 +0200

    Create Lecturer model
    Add a Lecturer model to differentiate lecturers and other users

---
+++ ContractMonthlyClaimSystem/Models/Lecturer.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContractMonthlyClaimSystem.Models
+{
+    public class Lecturer
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(200)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, EmailAddress, MaxLength(100)]
+        public string Email { get; set; } = string.Empty;
+
+        [Precision(18, 2)]
+        [Range(typeof(decimal), "0", "10000")]
+        public decimal HourlyRate { get; set; }
+
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+    }
+}
